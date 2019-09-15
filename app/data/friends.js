73569d409b3ba6999bda18bcd9bd6a3beacc5b07
ngiridhar1 <50300536+ngiridhar1@users.noreply.github.com>
2019-09-15 08:30:22 -0400

    Final push

---
+++ app/data/friends.js
@@ -0,0 +1,30 @@
+var friends = [
+	
+  {
+   "name":"Demi",
+   "photo":"https://d1b2ioo6u2o9wn.cloudfront.net/680x380/demi_lovato.jpg",
+   "scores":[5,1,4,4,5,1,2,5,4,1]
+   }, {
+   "name":"Obama",
+   "photo":"https://static.politico.com/dims4/default/2cd2450/2147483647/resize/1160x/quality/90/?url=http%3A%2F%2Fs3-origin-images.politico.com%2Fnews%2F101215_obama_smile_tax_reut_605.jp",
+   "scores":[5,1,4,4,5,1,2,5,4,1]
+   }, {
+   "name":"Ariana Grande",
+   "photo":"http://i.hurimg.com/i/hurriyet/75/620x350/5923bba1c03c0e248c782205.jpg",
+   "scores":[5,1,4,4,5,1,2,5,4,1]
+   }, {
+   "name":"Ryan Gosling",
+   "photo":"https://media.glamour.com/photos/582216cb6992121641ec2b21/master/w_644,c_limit/ryan-gosling-dad.jpg",
+   "scores":[5,1,4,4,5,1,2,5,4,1]
+   }, {
+   "name":"Tom Cruise",
+   "photo":"https://specials-images.forbesimg.com/imageserve/593b2e4b31358e03e55a0e8c/416x416.jpg?background=000000&cropX1=634&cropX2=2468&cropY1=39&cropY2=1874",
+   "scores":[5,1,4,4,5,1,2,5,4,1]
+   }, {
+   "name":"Cincinati",
+   "photo":"https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+   "scores":[5,1,4,4,5,1,2,5,4,1]
+   }
+ ];
+
+module.exports = friends;
